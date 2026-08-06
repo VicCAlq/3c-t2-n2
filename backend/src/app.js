d0d87866a95b97cfdc0aaf1f464a37b62b4499e3
Renan o Ruim <pedro.renan@nave.org.br>
2026-08-06 08:46:16 -0300

    Dicas adicionadas

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,47 +1,58 @@
-const express = require('express')
-const path = require('path')
-const cors = require('cors');
-const sql = require('sqlite3').verbose()
-const { 
+const express = require("express");
+const path = require("path");
+const cors = require("cors");
+const sql = require("sqlite3").verbose();
+const {
   porta,
   DB_NOME,
   TABELA_FONTES_NOME,
-  TABELA_NOTICIAS_NOME
-} = require('./env.js')
+  TABELA_NOTICIAS_NOME,
+} = require("./env.js");
 
-const app = express()
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
+
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(cors());
-app.use(express.static(path.join(__dirname, 'src')));
+app.use(express.static(path.join(__dirname, "src")));
 
-const db = new sql.Database(
-  `./${DB_NOME}`,
-  (erro) => {
-    if (erro) {
-      console.error(`Erro ao abrir o banco de dados "${DB_NOME}":`, erro.message);
-    } else {
-      console.log(`Conectado ao banco de dados SQLite3 "${DB_NOME}"`);
-    }
+
+const db = new sql.Database(`./${DB_NOME}`, (erro) => {
+  if (erro) {
+    console.error(`Erro ao abrir o banco de dados "${DB_NOME}":`, erro.message);
+  } else {
+    console.log(`Conectado ao banco de dados SQLite3 "${DB_NOME}"`);
   }
-)
+});
+
 
 db.run(
   `CREATE TABLE IF NOT EXISTS ${TABELA_FONTES_NOME} (
-    id INTEGER PRIMARY KEY AUTOINCREMENT
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nome TEXT NOT NULL,
+    link TEXT NOT NULL
   )`,
   (erro) => {
     if (erro) {
-      console.error(`Erro ao criar a tabela "${TABELA_FONTES_NOME}"`, erro.message);
+      console.error(
+        `Erro ao criar a tabela "${TABELA_FONTES_NOME}"`,
+        erro.message,
+      );
     } else {
       console.log(`Tabela "${TABELA_FONTES_NOME}" pronta!`);
     }
-  }
-)
+  },
+);
+
 
 db.run(
   `CREATE TABLE IF NOT EXISTS ${TABELA_NOTICIAS_NOME} (
-    id INTEGER PRIMARY KEY AUTOINCREMENT
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    titulo TEXT,
+    link TEXT UNIQUE,
+    descricao TEXT,
+    dataDePublicacao TEXT,
+    categorias TEXT
   )`,
   (erro) => {
     if (erro) {
@@ -52,28 +63,108 @@ db.run(
   }
 )
 
-app.get('/', (req, res) => {
+
+
+app.get("/", (req, res) => {
   res.status(200).json({
     message: "Acesso permitido",
     data: [],
     ok: true,
-  })
-})
-
-
-app.get('/api/noticias/categoria/:categoria', (req, res) => {
-  const categoria = req.params.categoria
-})
+  });
+});
 
 app.get('/api/fontes/cadastrar', (req, res) => {
+
   if (!req.query) {
     res.status(400).json({ error: erro.message });
     return
+  } else if (typeof(req.query.link) !== 'string') {
+    res.status(400).json({ error: `Propriedade "link" não é uma string válida` });
+  } else {
+    let url
+    try {
+      url = new URL(req.query.link)
+    } catch(err) {
+      res.status(400).json({ error: `O texto enviado não se trata de um endereço Web` });
+    }
   }
 
-  const { nome, endereco } = req.query
-})
+
+
+  const { nome, endereco } = req.query;
+
+
+  db.run(
+    `INSERT INTO ${TABELA_FONTES_NOME} (nome, endereco) VALUES (?, ?)`,
+    [nome, endereco],
+    (erro) => {
+      if (erro) {
+        res.status(400).json({ error: erro.message });
+        return;
+      } else {
+        res.status(201).json({
+          message: "Fonte cadastrada com sucesso",
+          data: { nome, endereco },
+        });
+      }
+    },
+  );
+});
+
+
+app.get("/api/fontes/filtrarFontes", (req, res) => {
+  db.all(
+    `SELECT * FROM ${TABELA_FONTES_NOME} WHERE endereco = ?`,
+    [req.query.endereco],
+    (erro, fontes) => {
+      if (erro) {
+        res.status(400).json({ error: erro.message });
+        return;
+      } else {
+        res.status(200).json({
+          message: "fonte filtradas com sucesso",
+          data: fontes,
+        });
+      }
+    },
+  );
+});
+
+
+app.get("/api/noticias/categoria/:categoria", (req, res) => {
+  const categoria = req.params.categoria;
+  db.all(
+    //Completa ae kkkkkkkkkkkkkkkkkk
+  );
+});
+
+
+app.delete("/api/fontes/deletar/fonte/:id", (req, res) => {
+  const id = req.params.id;
+  db.run(
+    `DELETE FROM ${TABELA_FONTES_NOME} WHERE id = ?`,
+    [id],
+    (erro) => {
+      if (erro) {
+        res.status(400).json({ error: erro.message });
+      } else {
+        res.status(200).json({
+          message: "Fonte deletada",
+        });
+      }
+    },
+  );
+});
+
+
+app.delete("/api/noticias/deletar/noticia/:id", (req, res) => {
+  const id = req.params.id;
+  db.run(
+    //Complete ae kkkkkkkk
+  );
+});
+
 
 app.listen(porta, () => {
-  console.log(`Servidor rodando em http://localhost:${porta}`)
-})
+  console.log(`Servidor rodando em http://localhost:${porta}`);
+});
